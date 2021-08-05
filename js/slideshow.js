b23898200932d74c0bf11816060e4f78d4d29f9c
Callum Metcalfe <callumjmetcalfe@outlook.com>
2021-08-05 15:26:01 +0100

    - made sure to actually add the rest of it

---
+++ js/slideshow.js
@@ -0,0 +1,16 @@
+var slideIndex = 0;
+showSlide();
+
+function showSlide(){
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length){
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlide, 5000);
+}
